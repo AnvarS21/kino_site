734a49c56399ba95d0438249f8bd61e37e1ac527
AnvarS21 <shamsutdinovanvar21@gmail.com>
2023-11-28 00:36:33 +0600

    added the main page and models

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
-
+from .models import Film, Serial
 # Register your models here.
+
+@admin.register(Film)
+class FilmsAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'desc',
+        'year',
+        'image',
+    )
+    list_display_links = (
+        'title',
+    )
+
+    search_fields = (
+
+        'title',
+        'desc',
+        'year',
+        'image',
+    )
+
+
+@admin.register(Serial)
+class SerialAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'desc',
+        'year',
+        'image',
+        'count_series',
+    )
+    list_display_links = (
+        'title',
+    )
+
+    search_fields = (
+
+        'title',
+        'desc',
+        'year',
+        'image',
+        'count_series',
+    )
--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Film(models.Model):
+    title = models.CharField(max_length=100,
+                             verbose_name='Название')
+    desc = models.TextField(verbose_name='Описание')
+    year = models.DateField(verbose_name='Год выпуска')
+    image = models.ImageField(upload_to='media/',
+                              verbose_name='Изображение')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Фильм'
+        verbose_name_plural = 'Фильмы'
+
+
+class Serial(models.Model):
+    title = models.CharField(max_length=100,
+                             verbose_name='Название')
+    desc = models.TextField(verbose_name='Описание')
+    year = models.DateField(verbose_name='Год выпуска')
+    image = models.ImageField(upload_to='media/',
+                              verbose_name='Изображение')
+    count_series = models.PositiveSmallIntegerField(verbose_name='Количество серий')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Сериал'
+        verbose_name_plural = 'Сериалы'
